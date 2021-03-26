40fdf25412442a93d5fffec3a4982ab562e0cc2e
Ervin Osuna <ervin.osuna@gmail.com>
2021-03-26 19:01:04 -0300

    cambio de puerto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // esto es un ejemplo
 const app = express();
 
-app.get('/',(req, res) => res.send('Hola mundo desde express'))
+app.get('/',(req, res) => res.send('<h1> express con HTML</h1>'))
 
-app.listen(3000);
-console.log('Server on port: 3000');
+app.listen(8080);
+console.log('Server on port: 8080');
